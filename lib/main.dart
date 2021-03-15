12f5b28e24294ace881139b6749cac9d182fe6b5
dhane31 <dhanegm731@gmail.com>
2021-03-15 10:18:20 +0900

    Add flutter_slidable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final todoProvider = ChangeNotifierProvider((ref) => Todo());
@@ -34,27 +35,55 @@ class MyHomePage extends HookWidget {
         title: Text("TODO LIST App"),
       ),
       body: Container(
-        child: ReorderableListView(
-          children: todoList.map((TodoModel todoModel) {
-            return Container(
-              key: Key(todoModel.getKey),
-              decoration: BoxDecoration(color: Colors.deepOrange[400]),
-              child: ListTile(
-                leading: Icon(
-                  todoModel.icon,
-                  color: Colors.white,
+        child: ListView.builder(
+            itemCount: todoList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Slidable(
+                actionExtentRatio: 0.2,
+                actionPane: SlidableScrollActionPane(),
+                actions: <Widget>[
+                  IconSlideAction(
+                    caption: 'Archive',
+                    color: Colors.lightBlue,
+                    icon: Icons.archive,
+                    onTap: () {},
+                  ),
+                  IconSlideAction(
+                    caption: 'share',
+                    color: Colors.indigo,
+                    icon: Icons.share,
+                    onTap: () {},
+                  ),
+                ],
+                secondaryActions: [
+                  IconSlideAction(
+                    caption: 'More',
+                    color: Colors.black45,
+                    icon: Icons.more_horiz,
+                    onTap: () {},
+                  ),
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    onTap: () {
+                      todo.deleteTodo(index);
+                    },
+                  ),
+                ],
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.deepOrange[400]),
+                  child: ListTile(
+                    leading: Icon(
+                      todoList[index].getIcon,
+                      color: Colors.white,
+                    ),
+                    title: Text(todoList[index].getTitle,
+                        style: TextStyle(color: Colors.white)),
+                  ),
                 ),
-                title: Text(
-                  todoModel.getTitle,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            );
-          }).toList(),
-          onReorder: (oldIndex, newIndex) {
-            todo.dragAndDrop(oldIndex, newIndex);
-          },
-        ),
+              );
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -65,13 +94,8 @@ class MyHomePage extends HookWidget {
 }
 
 class Todo with ChangeNotifier {
-  void dragAndDrop(int oldIndex, int newIndex) {
-    if (oldIndex < newIndex) {
-      newIndex -= 1;
-    }
-    TodoModel todoModel = todoList.removeAt(oldIndex);
-
-    todoList.insert(newIndex, todoModel);
+  void deleteTodo(int index) {
+    todoList.removeAt(index);
     notifyListeners();
   }
 }
